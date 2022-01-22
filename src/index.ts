20599e15c1a94662fa731f5c2662d6a0c980aeed
Hrushikesh Makode <makodehrushikesh@gmail.com>
2022-01-23 00:20:43 +0530

    init

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import { ApolloServer, gql } from "apollo-server-express";
+import express from "express";
+
+const typeDefs = gql`
+	type Book {
+		title: String
+		author: String
+	}
+
+	type Query {
+		books: [Book]
+	}
+`;
+
+const books = [
+	{
+		title: "The Awakening",
+		author: "Kate Chopin",
+	},
+	{
+		title: "City of Glass",
+		author: "Paul Auster",
+	},
+];
+
+const resolvers = {
+	Query: {
+		books: () => books,
+	},
+};
+
+const PORT = process.env.PORT || "4000";
+const app = express();
+const server = new ApolloServer({ typeDefs, resolvers });
+server.start().then((res) => {
+    server.applyMiddleware({ app });
+	app.listen({ port: PORT }, () => {
+		console.log(`🚀  Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+	});
+});
