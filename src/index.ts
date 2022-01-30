52cfc336b74b25627899ce11d59e259408468129
Hrushikesh Makode <makodehrushikesh@gmail.com>
2022-01-30 22:46:55 +0530

    check

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
-import express from  'express';
+import express from "express";
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+	return res.send({
+		Welcome: "Hrushikesh",
+	});
+});
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT,()=>{
-	console.log(`Server started at port ${PORT}`);
-})
+app.listen(PORT, () => {
+	console.log(`Server started at port http://localhost:${PORT}`);
+});
